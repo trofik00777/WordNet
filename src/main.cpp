68ef29a654702182f4bf6445258e4f7eb0b8a268
trofik00777 <maks.trofimov.0303@mail.ru>
2022-07-19 21:20:06 +0300

    add example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <fstream>
+#include <iostream>
 #include "wordnet.h"
 
 int main()
 {
+    std::ifstream synsets{"etc/synsets.txt"}, hypernyms{"etc/hypernyms.txt"};
+
+    std::cout << synsets.is_open() << "\n";
+
+    WordNet wordnet(synsets, hypernyms);
+    Outcast outcast(wordnet);
+    std::cout << outcast.outcast({"horse", "zebra", "cat", "bear", "table"}) << std::endl; // table
+
     return 0;
 }
